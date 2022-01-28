eca54555bd181a61f031276fbb4cc965048f6263
Anish <81043438+anish-ojas@users.noreply.github.com>
2022-01-28 22:40:30 +0530

    Typed Js

---
+++ src/app/pages/career/career.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CareerRoutingModule } from './career-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CareerRoutingModule
+  ]
+})
+export class CareerModule { }
--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-services',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent implements OnInit {
-
-  constructor() { }
+  displayWeb:boolean=false;
+  displayMarketing:boolean=false;
+  displayIot:boolean=false;
+  displayMobile:boolean=false;
+  constructor(private meta:Meta) {
+    this.meta.addTags([
+      { name: 'keywords', content: 'Web Application, UI/Front End Dev, Mobile App Dev, Mobile App, Testing Services' },
+      { name: 'description', content: 'Our expertise is in Web Application Development, UI/Front End Dev, Mobile App Dev, Mobile App Testing, Testing Services' },
+      { name: 'author', content: 'Ojas Enterprise' },
+    ]);
+   }
 
   ngOnInit(): void {
   }
-
+  showDevelopment(){
+    this.displayWeb=true;
+  }
+  showDigital(){
+    this.displayMarketing=true;
+  }
+  showIOT(){
+    this.displayIot=true;
+  }
+  showMobile(){
+    this.displayMobile=true;
+  }
 }
